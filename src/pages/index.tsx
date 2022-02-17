d1570588f62a2392a1b39794a06951d7344c403d
Benício Daniel <benicio.daniel@gmail.com>
2022-02-16 22:21:35 -0300

    Primeiro commit estruturando as pastas

---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center">
+      <Head>
+        <title>Instagram 2.0</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1>Hello</h1>
+
+      {/* Header */}
+
+      {/* Feed */}
+
+      {/* Modal */}
+    </div>
+  )
+}
