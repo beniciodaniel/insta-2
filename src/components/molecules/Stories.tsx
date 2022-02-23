04658a64b43b5a2709017838fb0d70b2e5481046
Benício Daniel <benicio.daniel@gmail.com>
2022-02-23 14:44:42 -0300

    Adicionou o próprio Story se estiver logado na lista de stories

--- src/components/molecules/Stories.tsx
+++ src/components/molecules/Stories.tsx
@@ -1,4 +1,5 @@
 import { ContextualCard } from '@faker-js/faker/helpers'
+import { useSession } from 'next-auth/react'
 import { useEffect, useState } from 'react'
 import { getFakerUsers } from '../../utils/getFakerUsers'
 import { Story } from '../atoms'
@@ -9,6 +10,7 @@ interface FakeUsersProps extends ContextualCard {
 
 export function Stories() {
   const [users, setUsers] = useState<FakeUsersProps[]>([])
+  const { data: session } = useSession()
 
   useEffect(() => {
     const fakeUsers = getFakerUsers(20)
@@ -17,6 +19,10 @@ export function Stories() {
 
   return (
     <div className="mt-8 flex space-x-2 overflow-x-scroll rounded-sm border border-gray-200 bg-white p-6 scrollbar-thin scrollbar-thumb-black">
+      {session && (
+        <Story image={session.user.image} username={session.user.username} />
+      )}
+
       {users.map((profile) => (
         <Story
           key={profile.id}
