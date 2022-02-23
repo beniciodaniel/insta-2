31fa685f4c880ca827c3387871b1f201eca1b41a
Benício Daniel <benicio.daniel@gmail.com>
2022-02-23 14:29:37 -0300

    Esconde sidebar (mini profile) se não estiver autenticado

--- src/components/molecules/MiniProfile.tsx
+++ src/components/molecules/MiniProfile.tsx
@@ -13,7 +13,7 @@ export function MiniProfile() {
         />
       )}
       <div className="mx-4 flex-1">
-        <h2 className="font-bold">beniciohasegawa</h2>
+        <h2 className="font-bold">{session?.user.username}</h2>
         <h3 className="text-sm text-gray-400">Welcome to Instagram</h3>
       </div>
 
--- src/components/organisms/Feed.tsx
+++ src/components/organisms/Feed.tsx
@@ -1,19 +1,28 @@
+import { useSession } from 'next-auth/react'
 import { MiniProfile, Posts, Stories, Suggestions } from '../molecules'
 
 export function Feed() {
+  const { data: session } = useSession()
+
   return (
-    <main className="mx-auto grid grid-cols-1 md:max-w-3xl md:grid-cols-2 xl:max-w-6xl xl:grid-cols-3">
+    <main
+      className={`mx-auto grid grid-cols-1 md:max-w-3xl md:grid-cols-2 xl:max-w-6xl xl:grid-cols-3 ${
+        !session && '!max-w-3xl !grid-cols-1'
+      }`}
+    >
       <section className="col-span-2">
         <Stories />
         <Posts />
       </section>
 
-      <section className="hidden md:col-span-1 xl:inline-grid">
-        <div className="fixed top-20 mt-14 ml-10 space-y-4">
-          <MiniProfile />
-          <Suggestions />
-        </div>
-      </section>
+      {session && (
+        <section className="hidden md:col-span-1 xl:inline-grid">
+          <div className="fixed top-20 mt-14 ml-10 space-y-4">
+            <MiniProfile />
+            <Suggestions />
+          </div>
+        </section>
+      )}
     </main>
   )
 }
