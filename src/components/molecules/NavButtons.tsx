19b97cf612b653f92c78be56f5a1399fb7be9049
Benício Daniel <benicio.daniel@gmail.com>
2022-02-22 21:26:30 -0300

    Adicionou o SessionProvider wrapper e estilizou SignInPage

--- src/components/molecules/NavButtons.tsx
+++ src/components/molecules/NavButtons.tsx
@@ -6,28 +6,40 @@ import {
   UserGroupIcon,
 } from '@heroicons/react/outline'
 import { HomeIcon } from '@heroicons/react/solid'
+import { signIn, signOut, useSession } from 'next-auth/react'
 
 export function NavButtons() {
+  const { data: session } = useSession()
+
   return (
     <div className="flex items-center justify-end space-x-4">
       <HomeIcon className="navButton" />
       <MenuIcon className="h-6 cursor-pointer md:hidden" />
 
-      <div className="navButton relative">
-        <PaperAirplaneIcon className="navButton rotate-45" />
-        <div className="absolute -top-1 -right-2 flex h-5 w-5 animate-pulse items-center justify-center rounded-full bg-red-500 text-xs text-white">
-          3
-        </div>
-      </div>
-      <PlusCircleIcon className="navButton" />
-      <UserGroupIcon className="navButton" />
-      <HeartIcon className="navButton" />
+      {session ? (
+        <>
+          <div className="navButton relative">
+            <PaperAirplaneIcon className="navButton rotate-45" />
+            <div className="absolute -top-1 -right-2 flex h-5 w-5 animate-pulse items-center justify-center rounded-full bg-red-500 text-xs text-white">
+              3
+            </div>
+          </div>
+          <PlusCircleIcon className="navButton" />
+          <UserGroupIcon className="navButton" />
+          <HeartIcon className="navButton" />
 
-      <img
-        className="h-10 cursor-pointer rounded-full"
-        src="https://avatars.githubusercontent.com/u/52285940?v=4"
-        alt="Profile pic"
-      />
+          {session?.user?.image && (
+            <img
+              onClick={() => signOut()}
+              className="h-10 w-10 cursor-pointer rounded-full"
+              src={session?.user?.image}
+              alt="Profile pic"
+            />
+          )}
+        </>
+      ) : (
+        <button onClick={() => signIn()}>Sign In</button>
+      )}
     </div>
   )
 }
--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -1,5 +1,6 @@
 import { GetServerSideProps } from 'next'
 import { getProviders, signIn } from 'next-auth/react'
+import { Header } from '../../components/organisms'
 
 interface Provider {
   name: string
@@ -13,13 +14,29 @@ interface Props {
 export default function SignIn({ providers }: Props) {
   return (
     <>
-      {Object.values(providers).map((provider) => (
-        <div key={provider.name}>
-          <button onClick={() => signIn(provider.id)}>
-            Sign in with {provider.name}
-          </button>
+      <Header />
+      <div className="-mt-56 flex min-h-screen flex-col items-center justify-center py-2 px-14 text-center">
+        <img
+          className="w-80"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/2880px-Instagram_logo.svg.png"
+          alt="Instagram logo"
+        />
+        <p className="font-xs italic">
+          This is not a real app, it is built for educational purposes only
+        </p>
+        <div className="mt-40">
+          {Object.values(providers).map((provider) => (
+            <div key={provider.name}>
+              <button
+                className="rounded-lg bg-blue-500 p-3 text-white"
+                onClick={() => signIn(provider.id, { callbackUrl: '/' })}
+              >
+                Sign in with {provider.name}
+              </button>
+            </div>
+          ))}
         </div>
-      ))}
+      </div>
     </>
   )
 }
